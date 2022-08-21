04eef597caf32dbb2e58a273a93174fa31988b88
RakhmankulovDjamshid <110743200+RakhmankulovDjamshid@users.noreply.github.com>
2022-08-22 01:57:50 +0500

    Ignore Binary

---
+++ SimulaTest/Example.cs
@@ -0,0 +1,25 @@
+﻿namespace SimulaTest;
+
+public class Example
+{
+    
+
+    private double _diametr;
+    private Balls ball;
+
+    public Example(double diametr, Balls ball)
+    {
+
+
+
+    }
+
+}
+
+
+ enum Balls
+{
+    Red,
+    Green,
+    White
+}
